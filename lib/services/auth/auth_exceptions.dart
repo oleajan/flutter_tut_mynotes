d52f3ff08df9d6d895c63bad778d60cf28fc14c1
Jan <olea.janrobert@gmail.com>
2022-03-23 18:55:47 +0800

    created proper auth (to be implimented)

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,13 @@
+// login exceptions
+class UserNotFoundAuthException implements Exception {}
+class WrongPasswordAuthException implements Exception {}
+
+// register exceptions
+class WeakPasswordAuthException implements Exception {}
+class EmailAlreadyInUserAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+// generic exceptions
+class GenericAuthException implements Exception {}
+class UserNotLoggedInAuthException implements Exception {}
